2fc47e3f765ed2e16e2c50757e868df297232a64
CarlosVJ27 <carlos.villalobos.jimenez@ucreativa.com>
2021-06-13 19:05:04 -0600

    first commit

--- script.js
+++ script.js
@@ -10,6 +10,26 @@ videoBox.onclick = function() {
   videoBox.setAttribute('class','hidden');
 };
 
-video.onclick = function() {
+video.onclick = function(event) {
+  event.stopPropagation();
   video.play();
 };
+
+const firstValue = document.querySelector('input.firstValue');
+const secondValue = document.querySelector('input.secondValue');
+const buttons = document.querySelector('#buttons');
+const result = document.querySelector('#result');
+
+buttons.onclick = function(e) {
+  switch (e.target.id) {
+    case 'add':
+      result.textContent = Number(firstValue.value) + Number(secondValue.value);
+      break;
+    case 'rest':
+      result.textContent = firstValue.value - secondValue.value;
+      break;
+    case 'multiply':
+      result.textContent = firstValue.value * secondValue.value;
+      break;
+  }
+}
